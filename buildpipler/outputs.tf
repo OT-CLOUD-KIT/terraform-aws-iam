b79d5c89e73fcb0ffffc63286b2b9588ee9dc6e2
Ashwani Singh <ashwani.singh@opstree.com>
2025-01-15 12:36:06 +0530

    Typo

--- buildpipler/outputs.tf
+++ buildpipler/outputs.tf
@@ -5,25 +5,25 @@ output "iam_role" {
 
 output "documentdb_role" {
   description = "value"
-  value = try(module.docdb_buildpiper_role.role null)
+  value = try(module.docdb_buildpiper_role.role, null)
 }
 
 output "msk_role" {
   description = "value"
-  value = try(module.msk_buildpiper_role.role null)
+  value = try(module.msk_buildpiper_role.role, null)
 }
 
 output "rds_role" {
   description = "value"
-  value = try(module.msk_buildpiper_role.role null)
+  value = try(module.msk_buildpiper_role.role, null)
 }
 
 output "common_role" {
   description = "value"
-  value = try(module.common_buildpiper_role.role null)
+  value = try(module.common_buildpiper_role.role, null)
 }
 
 output "buildpiper_instance_role" {
   description = "value"
-  value = try(module.buildpiper_role.role null)
+  value = try(module.buildpiper_role.role, null)
 }
